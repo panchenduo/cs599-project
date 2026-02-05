7a5fcd809d2ba9c964ef7bda3a9233a5b52e6904
panchenduo <panchenduo@163.com>
2026-02-05 18:54:14 +0800

    feature:过滤器需要放行多个接口，此处暂不实现，仅作代码标记，后续网关会实现

--- admin/src/main/java/com/wut/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/wut/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -20,7 +20,6 @@ import static com.wut.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NOT_AU
 public class UserTransmitFilter implements Filter {
     @Resource
     private final StringRedisTemplate stringRedisTemplate;
-
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
@@ -43,3 +42,62 @@ public class UserTransmitFilter implements Filter {
         }
     }
 }
+//package com.wut.shortlink.admin.common.biz.user;
+//
+//import cn.hutool.core.util.StrUtil;
+//import com.alibaba.fastjson2.JSON;
+//import com.google.common.collect.Lists;
+//import com.wut.shortlink.admin.common.convention.exception.ClientException;
+//import jakarta.annotation.Resource;
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//import static com.wut.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
+//import static com.wut.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NOT_AUTHORIZED;
+//
+///**
+// * 用户信息传输过滤器
+// */
+//@RequiredArgsConstructor
+//public class UserTransmitFilter implements Filter {
+//    @Resource
+//    private final StringRedisTemplate stringRedisTemplate;
+//    private static final List<String> IGNOREURLS = Lists.newArrayList("/api/short-link/admin/v1/user/has-username", "/api/short-link/admin/v1/user/login");
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+//        String requestURI = ((HttpServletRequest) servletRequest).getRequestURI();
+//        if (!IGNOREURLS.contains(requestURI)) { //不是登录的话进行拦截
+//            String method = httpServletRequest.getMethod();
+//            if (!requestURI.equals("/api/short-link/admin/v1/user") && method.equals("POST")) {
+//                String username = httpServletRequest.getHeader("username");
+//                String token = httpServletRequest.getHeader("token");
+//                if (!StrUtil.isAllBlank(username, token)) {
+//                    throw new ClientException(USER_NOT_AUTHORIZED);
+//                }
+//                Object userInfoStr;
+//                try {
+//                    userInfoStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
+//                    if (userInfoStr == null) {
+//                        throw new ClientException(USER_NOT_AUTHORIZED);
+//                    }
+//                } catch (Exception e) {
+//                    throw new ClientException(USER_NOT_AUTHORIZED);
+//                }
+//                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoStr.toString(), UserInfoDTO.class);
+//                UserContext.setUser(userInfoDTO);
+//                try {
+//                    filterChain.doFilter(servletRequest, servletResponse);
+//                } finally {
+//                    UserContext.removeUser();
+//                }
+//            }
+//        }
+//    }
+//}
