90d3cb2ef29272da75ff186dc117855b4adfcdf8
panchenduo <panchenduo@163.com>
2026-02-05 17:38:26 +0800

    feature:从admin模块添加通用代码到project模块

---
+++ project/src/main/java/com/wut/shortlink/project/common/database/BaseDO.java
@@ -0,0 +1,29 @@
+package com.wut.shortlink.project.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 数据库持久层对象基础属性
+ */
+@Data
+public class BaseDO {
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+    /**
+     * 逻辑删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private int delFlag;
+}
