dbd90de635373d105f30604f5b4a14a376e98603
panchenduo <panchenduo@163.com>
2026-02-27 23:43:04 +0800

    feature: 开发短链接分页监控相关功能

---
+++ project/src/main/java/com/wut/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,10 @@
+package com.wut.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wut.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * 短链接今日统计持久层
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
