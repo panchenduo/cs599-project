297bc4af43e0c81fb284b8ac373b2ab0837ea2c2
panchenduo <panchenduo@163.com>
2026-02-28 19:39:11 +0800

    fix: 修复短链接修改日期后无法访问问题

--- project/src/main/java/com/wut/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/wut/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -194,6 +194,15 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, LinkDO> i
             baseMapper.delete(updateWrapper);
             baseMapper.insert(shortLinkDO);
         }
+        if (!Objects.equals(hasShortLinkDO.getValidDateType(), requestParam.getValidDateType())
+                || !Objects.equals(hasShortLinkDO.getValidDate(), requestParam.getValidDate())) {
+            stringRedisTemplate.delete(String.format(GOTO_SHORT_LINK_KEY, requestParam.getFullShortUrl()));
+            if (hasShortLinkDO.getValidDate() != null && hasShortLinkDO.getValidDate().before(new Date())) {
+                if (Objects.equals(requestParam.getValidDateType(), ValiDateTypeEnum.PERMANENT.getType()) || requestParam.getValidDate().after(new Date())) {
+                    stringRedisTemplate.delete(String.format(GOTO_IS_NULL_SHORT_LINK_KEY, requestParam.getFullShortUrl()));
+                }
+            }
+        }
     }
 
     @SneakyThrows
