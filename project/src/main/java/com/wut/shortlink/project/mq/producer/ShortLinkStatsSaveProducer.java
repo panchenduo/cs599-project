da6fc95a54675058cfcba6b7bd742a4e32429972
panchenduo <panchenduo@163.com>
2026-03-04 13:13:23 +0800

    optimize: 保持代码规范将缓存 Key 移至常量类

--- project/src/main/java/com/wut/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/com/wut/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -2,11 +2,12 @@
 package com.wut.shortlink.project.mq.producer;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+
+import static com.wut.shortlink.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
 
 /**
  * 短链接监控状态保存消息队列生产者
@@ -17,13 +18,11 @@ public class ShortLinkStatsSaveProducer {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    @Value("${spring.data.redis.channel-topic.short-link-stats}")
-    private String topic;
 
     /**
      * 发送延迟消费短链接统计
      */
     public void send(Map<String, String> producerMap) {
-        stringRedisTemplate.opsForStream().add(topic, producerMap);
+        stringRedisTemplate.opsForStream().add(SHORT_LINK_STATS_STREAM_TOPIC_KEY, producerMap);
     }
 }
