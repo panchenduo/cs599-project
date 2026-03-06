6446a038b888b8f717ce9d525213babf1f431e53
panchenduo <panchenduo@163.com>
2026-03-06 11:47:11 +0800

    feature: 引入网关改造短链接服务

--- admin/src/main/java/com/wut/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/wut/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,107 +1,30 @@
-//package com.wut.shortlink.admin.common.biz.user;
-//
-//import com.alibaba.fastjson2.JSON;
-//import com.wut.shortlink.admin.common.convention.exception.ClientException;
-//import jakarta.annotation.Resource;
-//import jakarta.servlet.*;
-//import jakarta.servlet.http.HttpServletRequest;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.data.redis.core.StringRedisTemplate;
-//
-//import java.io.IOException;
-//
-//import static com.wut.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
-//import static com.wut.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NOT_AUTHORIZED;
-//
-///**
-// * 用户信息传输过滤器
-// */
-//@RequiredArgsConstructor
-//public class UserTransmitFilter implements Filter {
-//    @Resource
-//    private final StringRedisTemplate stringRedisTemplate;
-//    @Override
-//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-//        String requestURI = ((HttpServletRequest) servletRequest).getRequestURI();
-//        if (!requestURI.equals("/api/short-link/admin/v1/user/login")) { //不是登录的话进行拦截
-//            String username = httpServletRequest.getHeader("username");
-//            String token = httpServletRequest.getHeader("token");
-//            Object userInfoStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
-//            if (userInfoStr != null) {
-//                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoStr.toString(), UserInfoDTO.class);
-//                UserContext.setUser(userInfoDTO);
-//            } else {
-//                throw new ClientException(USER_NOT_AUTHORIZED);
-//            }
-//        }
-//        try {
-//            filterChain.doFilter(servletRequest, servletResponse);
-//        } finally {
-//            UserContext.removeUser();
-//        }
-//    }
-//}
 package com.wut.shortlink.admin.common.biz.user;
 
 import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson2.JSON;
-import com.google.common.collect.Lists;
-import jakarta.annotation.Resource;
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-
-import static com.wut.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
-import static com.wut.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NOT_AUTHORIZED;
+import lombok.SneakyThrows;
 
 /**
  * 用户信息传输过滤器
  */
 @RequiredArgsConstructor
-@Slf4j
 public class UserTransmitFilter implements Filter {
-    @Resource
-    private final StringRedisTemplate stringRedisTemplate;
-    private static final List<String> IGNOREURLS = Lists.newArrayList("/api/short-link/admin/v1/user/has-username", "/api/short-link/admin/v1/user/login");
 
+    @SneakyThrows
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String requestURI = ((HttpServletRequest) servletRequest).getRequestURI();
-        if (!IGNOREURLS.contains(requestURI)) {
-            //不是登录的话进行拦截
-            String method = httpServletRequest.getMethod();
-            if (!(requestURI.equals("/api/short-link/admin/v1/user") && method.equals("POST"))) {
-                String username = httpServletRequest.getHeader("username");
-                String token = httpServletRequest.getHeader("token");
-                if (StrUtil.isAllBlank(username, token)) {
-//                    throw new ClientException(USER_NOT_AUTHORIZED);
-                    returnJson(servletResponse, JSON.toJSONString(USER_NOT_AUTHORIZED));
-                    return;
-                }
-                Object userInfoStr;
-                try {
-                    userInfoStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
-                    if (userInfoStr == null) {
-//                        throw new ClientException(USER_NOT_AUTHORIZED);
-                        returnJson(servletResponse, JSON.toJSONString(USER_NOT_AUTHORIZED));
-                        return;
-                    }
-                } catch (Exception e) {
-//                    throw new ClientException(USER_NOT_AUTHORIZED);
-                    returnJson(servletResponse, JSON.toJSONString(USER_NOT_AUTHORIZED));
-                    return;
-                }
-                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoStr.toString(), UserInfoDTO.class);
-                UserContext.setUser(userInfoDTO);
-            }
+        String username = httpServletRequest.getHeader("username");
+        if (StrUtil.isNotBlank(username)) {
+            String userId = httpServletRequest.getHeader("userId");
+            String realName = httpServletRequest.getHeader("realName");
+            UserInfoDTO userInfoDTO = new UserInfoDTO(userId, username, realName);
+            UserContext.setUser(userInfoDTO);
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
@@ -109,21 +32,4 @@ public class UserTransmitFilter implements Filter {
             UserContext.removeUser();
         }
     }
-
-    private void returnJson(ServletResponse response, String json) {
-        PrintWriter writer = null;
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json; charset=utf-8");
-        try {
-            writer = response.getWriter();
-            writer.print(json);
-
-        } catch (IOException e) {
-            log.error("response error", e);
-        } finally {
-            if (writer != null)
-                writer.close();
-        }
-    }
-
 }
---
+++ gateway/src/main/java/com/wut/shortlink/gateway/GatewayServiceApplication.java
@@ -0,0 +1,16 @@
+
+package com.wut.shortlink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关服务应用启动器
+ */
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
