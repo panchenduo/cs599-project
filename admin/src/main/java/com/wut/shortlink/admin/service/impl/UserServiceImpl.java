a45e676874b3f5a5efcc235f650ce2823d2002b7
panchenduo <panchenduo@163.com>
2026-03-06 12:59:02 +0800

    feature: 优化用户登录接口续期逻辑

--- admin/src/main/java/com/wut/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/wut/shortlink/admin/service/impl/UserServiceImpl.java
@@ -118,6 +118,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         //多端登录
         Map<Object ,Object> hasLoginMap = stringRedisTemplate.opsForHash().entries(USER_LOGIN_KEY + userLoginReqDTO.getUsername());
         if (CollUtil.isNotEmpty(hasLoginMap)) {
+            stringRedisTemplate.expire(USER_LOGIN_KEY + userLoginReqDTO.getUsername(), 30L, TimeUnit.MINUTES);
             String token = hasLoginMap.keySet().stream()
                     .findFirst()
                     .map(Object::toString)
